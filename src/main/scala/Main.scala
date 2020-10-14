063eb10352c41954ce307e34eb1ecff30b7b5323
abdul rafay <rafayjangsher2@gmail.com>
2020-10-14 13:12:09 +0500

    spark read file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,34 @@
+import org.apache.spark
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
 
 
 object Main {
   def main(args:Array[String]):Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
-    println("hello")
-    println("another hello")
-    println("and another one again bro")
-    println("hi!")
+    val spark = org.apache.spark.sql.SparkSession.builder
+      .master("local")
+      .appName("Spark CSV Reader")
+      .getOrCreate;
+
+
+    val df = spark.read
+      .format("csv")
+      .option("header", "true") //first line in file has headers
+      .option("mode", "DROPMALFORMED")
+      .load("/Users/abdulrafay/PycharmProjects/cookiepedia_refined/Cookiepedia_scraped_data.csv")//hdfs:///csv/file/dir/file.csv
+      .toDF()
+
+    println(df.show(5))
+    println("\n\n")
+
+      for (row <- df.take(2)){
+        println("Row"+row.getClass)
+        return
+      }
 
   }
 
